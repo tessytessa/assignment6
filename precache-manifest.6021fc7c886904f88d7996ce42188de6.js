b3ecbd4840b0e54a20a0f686e6788e5d351e19ce
tessytessa <lovietessa06@gmail.com>
2020-09-11 09:42:52 +0000

    Updates

---
+++ precache-manifest.6021fc7c886904f88d7996ce42188de6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59dc554ef3216dc29b253991b989a37e",
+    "url": "/assignment6/index.html"
+  },
+  {
+    "revision": "37dab6390851bb7753ac",
+    "url": "/assignment6/static/css/main.0a0650df.chunk.css"
+  },
+  {
+    "revision": "d2e373c254585c9be00e",
+    "url": "/assignment6/static/js/2.a7f2996d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/assignment6/static/js/2.a7f2996d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "37dab6390851bb7753ac",
+    "url": "/assignment6/static/js/main.1bfdc94e.chunk.js"
+  },
+  {
+    "revision": "b82c1691da2acd6b4eee",
+    "url": "/assignment6/static/js/runtime-main.155e4637.js"
+  },
+  {
+    "revision": "6c5827cc271d60c0b21a94f2090910f2",
+    "url": "/assignment6/static/media/logo.6c5827cc.PNG"
+  }
+]);
